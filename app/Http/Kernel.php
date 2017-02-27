b7eaad54f5eead2b8f31649cccab5adb4627f285
Carlos Cuamatzin <kuamatzin@gmail.com>
2017-02-27 11:04:20 -0600

    Middlewares

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -56,5 +56,7 @@ class Kernel extends HttpKernel
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
+        'manager' => \App\Http\Middleware\RedirectIfNotAManager::class,
+        'analista' => \App\Http\Middleware\RedirectIfNotAnalista::class,
     ];
 }
