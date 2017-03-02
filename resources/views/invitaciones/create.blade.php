bd1805eb0fe373b71604f8ac7bf57423814fab0b
Carlos Cuamatzin <kuamatzin@gmail.com>
2017-03-02 12:33:29 -0600

    Clave en crear invitacion 2

--- resources/views/invitaciones/create.blade.php
+++ resources/views/invitaciones/create.blade.php
@@ -194,6 +194,13 @@
                             <small class="text-danger">{{ $errors->first($partida->id) }}</small>
                         </div>
                         @else
+
+                        <div class="form-group col-md-2 @if($errors->first($partida->id)) has-error @endif">
+                            {!! Form::label($partida->id, 'Clave o Modelo') !!}
+                            {!! Form::text($partida->id . '_clave', $ofertas_proveedor->where('partida_id', $partida->id)->first()->clave, ['class' => 'form-control', 'required' => 'required', 'id' => $key . '_clave', 'readonly']) !!}
+                            <small class="text-danger">{{ $errors->first($partida->id) }}</small>
+                        </div>
+                        
                         <div class="form-group col-md-2 @if($errors->first($partida->id)) has-error @endif">
                             {!! Form::label($partida->id, 'Marca') !!}
                             {!! Form::text($partida->id . '_marca', $partida->marca, ['class' => 'form-control', 'required' => 'required', 'id' => $key . '_marca', 'readonly']) !!}
